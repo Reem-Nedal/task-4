076c832c00da799f3e1b88f1d727d6316d75288d
Reem-Nedal <96889421+Reem-Nedal@users.noreply.github.com>
2022-02-18 03:20:55 +0200

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,56 @@
+
+
+//array of colors
+
+let arrayofcolors=["red","blue","green","yellow"];
+
+console.log(arrayofcolors[0]);
+console.log(arrayofcolors[1]);
+console.log(arrayofcolors[2]);
+console.log(arrayofcolors[3]);
+
+//random value from 0 to 255
+
+ var randomone =Math.floor((Math.random() * 255)+0);
+  console.log(randomone) ;
+
+  let randomtwo = Math.floor(Math.random() * 255);
+  console.log(randomtwo );
+
+  let randomthree=Math.random() * 255 ;
+  console.log(randomthree);
+
+//ranodm value
+
+  let randomfour = Math.random();
+  console.log(randomfour);
+
+//function
+  function names()
+  
+  {
+let a=88;
+let b=77;
+let c=66;
+return [a,b,c] ;
+
+ }
+
+ console.log( names());
+
+
+//object car 
+
+const car={
+
+name:"mercedes",
+color:"white",
+price:"50000",
+
+};
+
+console.log(car.name);
+console.log(car.color);
+console.log(car.price);
+
+
